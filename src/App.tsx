2326bf36d89a0af24b79bc948323757b64151346
Adam Murphy <adammurphy10@gmail.com>
2026-04-03 21:10:05 +0100

    Increase logo size and nudge down

--- src/App.tsx
+++ src/App.tsx
@@ -128,7 +128,7 @@ export default function App() {
             <img
               src="/trackman-logo.png"
               alt="TrackMan"
-              className="inline-block h-40 sm:h-44 md:h-48 w-auto align-middle mix-blend-lighten -my-14 sm:-my-16 mr-2"
+              className="inline-block h-52 sm:h-56 md:h-60 w-auto align-middle mix-blend-lighten -my-16 sm:-my-20 translate-y-3 mr-2"
             />{' '}Facility.
           </h2>
           <p className="text-base sm:text-lg text-white/50 font-light leading-relaxed max-w-md mx-auto">
